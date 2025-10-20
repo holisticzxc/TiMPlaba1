a5db111dcc7809ce17696cc5dd7d7743216d1352
holisticzxc <egorkotik86@gmail.com>
2025-10-20 21:52:27 +0300

    Update modAlphaCipher.h

--- 1_1/modAlphaCipher.h
+++ 1_1/modAlphaCipher.h
@@ -3,6 +3,13 @@
 
 #include <string>
 #include <vector>
+#include <stdexcept>
+
+class cipher_error : public std::invalid_argument {
+public:
+    explicit cipher_error(const std::string& what_arg) : std::invalid_argument(what_arg) {}
+    explicit cipher_error(const char* what_arg) : std::invalid_argument(what_arg) {}
+};
 
 class modAlphaCipher
 {
@@ -10,6 +17,7 @@ private:
     const std::string numAlpha = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
     std::vector<int> key;
 
+    std::string removeSpaces(const std::string& s) const;
     std::vector<int> textToIndices(const std::string& text) const;
     std::string indicesToText(const std::vector<int>& indices) const;
 
